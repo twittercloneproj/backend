b25d8f06ba0397a4e1e981cf02143adb6dfab7cb
MurgaNS <nemanja.murgaski@gmail.com>
2022-11-18 22:12:33 +0100

    Omitempty added to all fields in user struct.

--- auth_service/data/user.go
+++ auth_service/data/user.go
@@ -9,17 +9,17 @@ import (
 
 type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name     string             `bson:"name" json:"name" `
-	Surname  string             `bson:"surname" json:"surname"`
-	Username string             `bson:"username" json:"username"`
-	Password string             `bson:"password" json:"password"`
-	Sex      string             `bson:"sex" json:"sex"`
-	Age      float32            `bson:"age" json:"age"`
-	Town     string             `bson:"town" json:"town"`
-	Email    string             `bson:"email" json:"email"`
-	Firm     string             `bson:"firm" json:"firm"`
-	Website  string             `bson:"website" json:"website"`
-	Role     string             `bson:"role" json:"role"`
+	Name     string             `bson:"name,omitempty" json:"name" `
+	Surname  string             `bson:"surname,omitempty" json:"surname"`
+	Username string             `bson:"username,omitempty" json:"username"`
+	Password string             `bson:"password,omitempty" json:"password"`
+	Sex      string             `bson:"sex,omitempty" json:"sex"`
+	Age      float32            `bson:"age,omitempty" json:"age"`
+	Town     string             `bson:"town,omitempty" json:"town"`
+	Email    string             `bson:"email,omitempty" json:"email"`
+	Firm     string             `bson:"firm,omitempty" json:"firm"`
+	Website  string             `bson:"website,omitempty" json:"website"`
+	Role     string             `bson:"role,omitempty" json:"role"`
 }
 
 //type BusinessUser struct {
